3d98cc92c83cd7c78bbb4d51ff80984534211db3
Jagannnath Gouda <70815042+jagannathgouda2000@users.noreply.github.com>
2023-08-16 22:38:17 +0530

    verifyOwner

---
+++ middlewares/verifyOwner.js
@@ -0,0 +1,12 @@
+const verifyOwner = async (req, res, next) => {
+  try {
+    if (req.userInfo && req.userInfo.roleId == 2) {
+      next();
+    } else {
+      return res.status(400).json({ message: "Forbidden" });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
